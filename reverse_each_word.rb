23f79e24331da2d17def25d4b3d0972115b633e0
J <jennifer.singleton@gmail.com>
2019-01-13 19:53:37 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,8 @@
 def reverse_each_word(sentence)
   sentence_array = sentence.split(" ")
   #new_array = []
-  sentence_array.collect do |word|
+  new_array = sentence_array.collect do |word|
     word.reverse
   end
-#  sentence_array.join(" ")
+  new_array.join(" ")
 end
